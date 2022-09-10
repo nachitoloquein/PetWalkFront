3f2a17330bf0c8ccd7f0d4b3f7429395d98a7be9
th3fxcker <luchitoxpatrick13@gmail.com>
2022-09-09 21:56:30 -0300

    validaciones de formulario

--- src/app/paseador/register/register.page.ts
+++ src/app/paseador/register/register.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, NgForm } from '@angular/forms';
+import { FormBuilder, FormControl, FormControlName, FormGroup, NgForm, Validators } from '@angular/forms';
 import { AlertController } from '@ionic/angular';
 import { TrabajadorService } from 'src/app/services/trabajador.service';
 import { ComunasService } from 'src/app/services/comunas.service';
@@ -17,6 +17,8 @@ export class RegisterPage implements OnInit {
       this.getComunas();
   }
 
+
+
   getComunas(){
     this.comunaService.listarComunas().subscribe(
       res => {
@@ -26,31 +28,69 @@ export class RegisterPage implements OnInit {
     );
   }
 
-  async presentAlert() {
+  async SubmitAlert() {
     const alert = await this.alertController.create({
       header: 'Solicitud Enviada',
       subHeader: '',
       message: 'su solicitud esta siendo revisada,esto puede tardar 2 dias habiles',
       buttons: ['OK'],
     });
-
     await alert.present();
+  }
+
+  async RequiredDataAlert(){
+    const alert = await this.alertController.create({
+      header: 'Faltan Datos',
+      message: 'Debe llenar todos los campos antes de enviar su solicitud',
+      buttons: ['OK']
+    })
+    await alert.present()
   }
 
   ngOnInit() {
   }
 
+  //agrega los registros del paseador y revuelve un mensaje de enviado o no envido, ademas de su validacion de campos vacios
   addRegistro(form: NgForm){
     try{
     this.trabajadorService.crearSolicitud(form.value).subscribe(
-      res =>
-       console.log(res),
-      err => console.log(err)
+      res => {
+        console.log(res);
+        if(form.valid){
+          this.SubmitAlert();
+          this.limpiarLogin(form)
+        }else{
+          this.RequiredDataAlert()
+        }
+      },
+      err => { 
+        console.log(err)}
     )
   }
   catch(err){
     alert(err)
   }
   }
+
+  // Resetear los datos al momento de realizar el submit
+  limpiarLogin(form:NgForm){
+    form.onReset()
+  }
+  //Valida los registros de
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
