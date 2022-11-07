51640d26e6a8733634669d38f1d9f71150813890
th3fxcker <luchitoxpatrick13@gmail.com>
2022-11-06 22:50:38 -0300

    error de validacion login

--- src/app/consumidor/consumidor.page.ts
+++ src/app/consumidor/consumidor.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ConsumidorService} from '../services/consumidor.service'
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -14,26 +15,47 @@ export class ConsumidorPage implements OnInit {
   contrasena: string;
 
   constructor(
+    private alertController : AlertController,
     private consumidorService: ConsumidorService, 
     private router : Router,
   ){}
 
   ngOnInit() {
+    
   }
 
 
   login(){
     try{
+      
     this.consumidorService.Logear(this.correo, this.contrasena).subscribe(
+      
       res=>{
+        
         console.log(res);
         localStorage.setItem('token', res['token']);
         this.router.navigate(['/perfil'])
     }, 
-      err=>console.log(err));
+      err=>{console.log(err),
+        this.ValidarCuenta(err);});
     }catch(error){
       alert(error)
     }
+  }
+
+  ValidarCuenta(err){
+    if(err.status == 401){
+      this.Mensaje('Usuario o Contraseña Incorrecto', '')
+    }
+  }
+
+  async Mensaje(headerMensaje: string, bodyMensaje: string){
+    const alert = await this.alertController.create({
+      header: headerMensaje,
+      message: bodyMensaje,
+      buttons: ['OK']
+    })
+    await alert.present();
   }
 
   
