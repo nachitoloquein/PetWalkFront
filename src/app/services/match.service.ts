4a4ee3e3c25ddbe8b77a938fe154b482dd96fa77
nachito <elnachoescachondo@gmail.com>
2022-11-08 13:47:06 -0300

    creacionServicio

---
+++ src/app/services/match.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { dominio } from '../conexion';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchService {
+
+  URL_API = `${dominio}/api/match`
+  constructor(private http: HttpClient) { }
+
+  verMatchConsumidor(id: string){
+    return this.http.get(`${this.URL_API}/consumidor/${id}`);
+  }
+
+  hacerMatch(idConsumidor: string, idTrabajador: string, horaTrabajo: string, monto:number){
+    return this.http.post(`${this.URL_API}`, {idConsumidor, idTrabajador, horaTrabajo, monto});
+  }
+
+}
